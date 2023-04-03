7e7cf1e6f39556943e0c8685b6d3e7dbbb698553
ekihaiyalulya <106852668+ekihaiyalulya@users.noreply.github.com>
2023-04-03 15:15:40 +0700

    Update backend_acacia_.py

--- backend_acacia_.py
+++ backend_acacia_.py
@@ -27,7 +27,7 @@ from werkzeug.utils import secure_filename
 from pyngrok import ngrok
 
 import subprocess
-subprocess.run(['!mkdir -p ~/.ngrok2 && cp /tmp/ngrok/ngrok ~/.ngrok2/ && chmod +x ~/.ngrok2/ngrok'])
+subprocess.run('!mkdir -p ~/.ngrok2 && cp /tmp/ngrok/ngrok ~/.ngrok2/ && chmod +x ~/.ngrok2/ngrok')
 
 ngrok.set_auth_token("2LonMZbaS41s6CASLcEM7yJ8kdQ_VJBpynu3ShKjaVsHuQBK")
 
